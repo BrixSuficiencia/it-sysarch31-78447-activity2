eb3ef8ae05143f2abf3831992ec580c20578d543
unknown <brixsuficiencia@gmail.com>
2024-03-03 13:15:44 +0800

    Updated

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,12 +1,21 @@
+/* eslint-disable react/jsx-key */
 import Header from "./Header.jsx"
 import Footer from "./Footer.jsx"
 import Student from "./Student.jsx"
 
 function App() {
+  const myArray = [<Student/>,<Student/>,<Student/>,<Student/>,<Student/>];
+
     return(
       <>
         <Header/>
-        <Student/>
+        <div>
+          <ul className="loop">
+            {myArray.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        </div>
         <Footer/>
       </>
     )
--- my-react-app/src/Student.jsx
+++ my-react-app/src/Student.jsx
@@ -1,15 +1,12 @@
 
 function Student(){
-    <div id="student">
-                <img id="pic" src="https://cdn-icons-png.freepik.com/256/552/552848.png" alt="profile picture"></img>
-                <h2 id="id">John Doe</h2>
-                <p id="email">john@doe.com</p>
-    </div>
-    const studlist = (student, student);
-
-    const list = studlist.map(student => <li>{student}</li>)
+    
     return(
-        <ul>{list}</ul>
+        <div className="student">
+                <img className="pic" src="https://cdn-icons-png.freepik.com/256/552/552848.png" alt="profile picture"></img>
+                <h2 className="id">John Doe</h2>
+                <p className="email">john@doe.com</p>
+        </div>
     );
 }
 
